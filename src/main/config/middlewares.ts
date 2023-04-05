94d60844d5c55529f876cf944f0d2cc72f7a3bbd
Cesarzoleko <cesarzoleko@gmail.com>
2023-04-05 12:15:25 +0200

    add bodyParser

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -3,12 +3,14 @@ import { cors } from "@main/middlewares/cors"
 import { contentType } from "@main/middlewares/content-type";
 import {compress }from "@main/middlewares/compression";
 import { useMorgan } from "@main/middlewares/morgan";
+import { bodyParser } from "@main/middlewares/body-parser";
 import { authMiddleware } from "@main/middlewares/auth-middleware";
 
 
 export const  useMiddleware = (app : Express) =>{
     app.use(cors); 
     app.use(contentType);
+    app.use(bodyParser);
     app.use(compress);
     app.use(useMorgan)
     app.use(authMiddleware)
